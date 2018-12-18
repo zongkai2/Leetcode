0256c9853f133ebba911023f215e41c99d326b78
zongkai2 <94284751@qq.com>
2018-12-18 15:00:07 +0800

    add some src

--- LeetCode/LeetCode/pro2.cpp
+++ LeetCode/LeetCode/pro2.cpp
@@ -41,4 +41,109 @@ ListNode* pro2::addTwoNumbers(ListNode* l1, ListNode* l2)
 		ans->next = last_node;
 	}
 	return head;
-}				
+}	
+bool pro2::is_HaveSameNum(string s, int start, int end)
+{
+	bool ans = false;
+	int i, j;
+	for (i = start; i < end; i++)
+	{
+		for (j = i + 1; j < end; j++)
+		{
+			if (s[i] == s[j])
+				ans = true;
+		}
+	}
+	return ans;
+}
+int pro2::lengthOfLongestSubstring(string s)
+{
+	int i, j, temp, count,ans=0,k;
+	count = s.length();
+	ans = 0;
+	if (s == "")
+	{
+		return 0;
+	}
+	for (i=0;i<count;i++)
+	{
+		temp = 1;
+		while (!is_HaveSameNum(s,i,i+temp) && (i+temp)<=count)
+		{
+			if (ans < temp)
+			{
+				ans = temp;
+			}
+			temp++;				
+		}	
+	}
+	return ans;
+}
+int pro2::lengthOfLongestSubstring2(string s)
+{
+	int len = s.length();
+	int hash[256];
+	memset(hash, 0, sizeof(hash));
+	int maxlen = 0, index = 0;
+	int temp, i;
+	for (i = 0; i<len; i++) {
+		if (hash[s[i]]>index) {
+			temp = i - index;
+			if (temp>maxlen) {
+				maxlen = temp;
+			}
+			index = hash[s[i]];
+			hash[s[i]] = i + 1;  //更新发现的重复符号的索引
+		}
+		else {
+			hash[s[i]] = i + 1;
+		}
+	}
+	temp = i - index;
+	if (temp>maxlen) {
+		maxlen = temp;
+	}
+	return maxlen;
+}
+vector<int>& pro2::sortNum(vector<int>& nums)
+{
+	vector<int>& ans=nums;
+	int temp;
+	int count = ans.size();
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = i + 1; j < count; j++)
+		{
+			if (ans[i] > ans[j])
+			{
+				temp = ans[i];
+				ans[i] = ans[j];
+				ans[j] = temp;
+			}
+		}
+	}
+	return ans;
+}
+double pro2::findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2)
+{
+	vector<int>& T1 = nums1;
+	vector<int>& T2 = nums2;
+	int num1_size = T1.size();
+	int num2_size = T2.size();
+	int num_sum = num1_size + num2_size;
+	double ans;
+	for (vector<int>::size_type ix = 0; ix != num2_size; ix++)
+	{
+		T1.push_back(T2[ix]);
+	}
+	vector<int>&sort_nums = sortNum(T1);
+	if (num_sum %2)
+	{
+		ans= sort_nums[((num_sum-1)*1.0/2)];
+	}
+	else
+	{
+		ans=  (sort_nums[(num_sum - 1)/2] + sort_nums[(num_sum + 1)/2]) / 2.0;
+	}
+	return ans;
+}
--- LeetCode/LeetCode/pro2.h
+++ LeetCode/LeetCode/pro2.h
@@ -13,4 +13,9 @@ struct ListNode {
 class pro2 {
 public:
 	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2);
+	int lengthOfLongestSubstring(string s);
+	int lengthOfLongestSubstring2(string s);
+	bool is_HaveSameNum(string s,int start,int end);
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2);
+	vector<int>& sortNum(vector<int>& nums);
 };
